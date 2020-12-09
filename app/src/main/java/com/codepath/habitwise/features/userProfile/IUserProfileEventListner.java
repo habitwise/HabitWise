a35d2b581a526db9e8c814a922dffe254757f425
anthonyhom2 <ahom2@buffalo.edu>
2020-12-08 22:11:06 -0500

    Added and displayed friend request section. User can confirm or decline a request. XML still looks weird. Still need to add user search and update recycler view for friend requests.

--- app/src/main/java/com/codepath/habitwise/features/userProfile/IUserProfileEventListner.java
+++ app/src/main/java/com/codepath/habitwise/features/userProfile/IUserProfileEventListner.java
@@ -1,5 +1,6 @@
 package com.codepath.habitwise.features.userProfile;
 
+import com.codepath.habitwise.models.Friends;
 import com.parse.ParseUser;
 
 import java.util.List;
@@ -8,4 +9,8 @@ public interface IUserProfileEventListner {
     void fetchFriendsListSuccessful(List<ParseUser> newFriendsList);
     void fetchFriendsListFailed(Exception e);
     void updateRvFriendsList();
+
+    void fetchFriendRequestsSuccessful(List<Friends> newFriendRequests);
+    void fetchFriendRequestsFailed(Exception e);
+    void updateRvFriendRequests();
 }
--- app/src/main/java/com/codepath/habitwise/features/userProfile/IUserProfileRepository.java
+++ app/src/main/java/com/codepath/habitwise/features/userProfile/IUserProfileRepository.java
@@ -1,9 +1,7 @@
 package com.codepath.habitwise.features.userProfile;
 
-import com.parse.ParseUser;
-
-import java.util.List;
-
 public interface IUserProfileRepository {
     void fetchFriendsList(IUserProfileEventListner eventListner);
+
+    void fetchFriendRequests(IUserProfileEventListner eventListner);
 }
